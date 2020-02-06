3beb412378d2f1e81cfb6f0f17e1403bbcb69a01
Kid-Justice <drew.jennings@digipen.edu>
2020-02-05 16:35:57 -0800

    Made Score And Highscore Work

--- Project/DinoRun/Assets/Scripts/GameManager.cs
+++ Project/DinoRun/Assets/Scripts/GameManager.cs
@@ -7,15 +7,28 @@ public class GameManager : MonoBehaviour
 {
     public bool GameActive = false;
     public bool GameOver = false;
+    public int Score = 0;
+    public int Highscore = 0;
+    public float ScoreSpeed = 0.1f;
+    public float timer = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Highscore = PlayerPrefs.GetInt("Highscore", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+        if (timer > ScoreSpeed)
+        {
+            if (GameActive)
+            {
+                Score++;
+                timer = 0f;
+            }
+        }
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
         {
             if (!GameOver)
@@ -24,6 +37,10 @@ public class GameManager : MonoBehaviour
             }
             else
             {
+                if (PlayerPrefs.GetInt("Highscore", 0) < Score)
+                {
+                    PlayerPrefs.SetInt("Highscore", Score);
+                }
                 SceneManager.LoadScene("Game");
             }
             
---
+++ Project/DinoRun/Assets/Scripts/Scores.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scores : MonoBehaviour
+{
+    Text text;
+    public GameObject GMOB;
+    public bool isHighscore;
+    int Highscore;
+    GameManager GM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+        GM = GMOB.GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Highscore = GM.Highscore;
+        if (isHighscore)
+        {
+            if (Highscore >= 10000)
+            {
+                text.text = "" + Highscore;
+            }
+            else if (Highscore >= 1000)
+            {
+                text.text = "0" + Highscore;
+            }
+            else if (Highscore >= 100)
+            {
+                text.text = "00" + Highscore;
+            }
+            else if (Highscore >= 10)
+            {
+                text.text = "000" + Highscore;
+            }
+            else
+            {
+                text.text = "0000" + Highscore;
+            }
+        }
+        else
+        {
+            if (GM.Score >= 10000)
+            {
+                text.text = "" + GM.Score;
+            }
+            else if (GM.Score >= 1000)
+            {
+                text.text = "0" + GM.Score;
+            }
+            else if (GM.Score >= 100)
+            {
+                text.text = "00" + GM.Score;
+            }
+            else if (GM.Score >= 10)
+            {
+                text.text = "000" + GM.Score;
+            }
+            else
+            {
+                text.text = "0000" + GM.Score;
+            }
+        }
+    }
+}
