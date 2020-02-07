77be4748cf21bdb3f2d50c7d21f626f0d53a5558
hugo-autio-digipen <56566836+hugo-autio-digipen@users.noreply.github.com>
2020-02-06 17:19:17 -0800

    -Spawner and Sound

---
+++ Project/DinoRun/Assets/Scripts/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    Text text;
+    GameManager GM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GM.GameOver == true)
+        {
+            text.text = "Game Over";
+        }
+            
+    }
+}
