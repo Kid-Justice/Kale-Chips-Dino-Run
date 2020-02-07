4a4181cf45a46c20dc3035e2031c5feed8180606
hugo-autio-digipen <56566836+hugo-autio-digipen@users.noreply.github.com>
2020-02-06 17:53:42 -0800

    -Minor Changes

--- Project/DinoRun/Assets/Scripts/Obstacle.cs
+++ Project/DinoRun/Assets/Scripts/Obstacle.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
 
 public class Obstacle : MonoBehaviour
 {
+    Animator anim;
+    public bool IsBird;
     public float speed = 0.1f;
     GameManager GM;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         GM = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
@@ -18,6 +22,10 @@ public class Obstacle : MonoBehaviour
         if (GM.GameActive)
         {
             transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+        if(IsBird == true)
+        {
+            anim.SetInteger("State", 0);
         }
     }
 }
